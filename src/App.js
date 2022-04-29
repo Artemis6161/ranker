19e3a54d08eb60df9b28928c90cf26c1af005284
sangeetha <Artemis6161@github.com>
2022-04-29 14:34:21 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './header'
+import Body1 from './body1'
+import Body2 from './body2'
+import Body3 from './body3'
+import Body4 from './body4'
+import Footer from './footer'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+     
+   <Header/>
+   <Body1/>
+   <Body2/>
+   <Body3/>
+   <Body4/>
+
+   <Footer/>
+    </div> 
+  )
 }
 
-export default App;
+export default App
+
